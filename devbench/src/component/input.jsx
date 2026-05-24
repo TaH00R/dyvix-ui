512d1375a8b93a26bf4d5f52b78499263ad20bb5
younisdev <younisayoub10@gmail.com>
2026-05-25 02:33:37 +0300

    add input component disabled styles

--- devbench/src/component/input.jsx
+++ devbench/src/component/input.jsx
@@ -5,7 +5,7 @@ export function InputTest() {
     <>
       <DyvixInput
         animation={DYVIX_GLOBAL_ANIMATION.AURORA}
-        theme={DYVIX_GLOBAL_THEME.SINGULARITY}
+       theme={DYVIX_GLOBAL_THEME.SINGULARITY}
         type="text"
         placeholder={'hi to'}
       />
