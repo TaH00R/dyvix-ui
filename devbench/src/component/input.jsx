a910d384c8751a6339a879d8e754d7019cc46944
younisdev <younisayoub10@gmail.com>
2026-05-27 02:03:48 +0300

    Add aurora input theme.

--- devbench/src/component/input.jsx
+++ devbench/src/component/input.jsx
@@ -5,7 +5,7 @@ export function InputTest() {
     <>
       <DyvixInput
         animation={DYVIX_GLOBAL_ANIMATION.AURORA}
-        theme={DYVIX_GLOBAL_THEME.NEON}
+        theme={DYVIX_GLOBAL_THEME.AURORA}
         type="text"
         placeholder={'Enter your name'}
       />
